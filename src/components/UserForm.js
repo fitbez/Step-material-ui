a05ea991707d2d2d886ca55f3e4c30ae234a491b
fitsum belayneh <fitsu_alt@yahoo.com>
2018-11-06 10:53:56 -0500

    adding methods for UserForm component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -10,8 +10,47 @@ export class UserForm extends Component {
     city: "",
     bio: ""
   };
+
+  // proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to prev step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fiels change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
   render() {
-    return <div />;
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+    }
   }
 }
 
