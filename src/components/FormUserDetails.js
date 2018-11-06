48dc12e96f49e796ef6de0a708517a3925b0f20d
fitsum belayneh <fitsu_alt@yahoo.com>
2018-11-06 11:32:46 -0500

    different commponents are add from  Material-ui to form user details

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
 export class FormUserDetails extends Component {
+  continue = e => {
+    e.preventDefualt();
+    this.props.nextStep();
+  };
   render() {
+    const { values } = this.props;
     return (
-      <div>
-        <h1>Hello from user details</h1>
-      </div>
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter User Details" />
+        </React.Fragment>
+      </MuiThemeProvider>
     );
   }
 }
